0cf7b9214a6721cddf6ced4dbea13228218b253c
mtgorski <mgorski8@gmail.com>
2015-12-29 10:10:16 -0500

    Added unit test for api project

---
+++ src/BlockWars.GameState.Api.Unit.Tests/Controllers/RealmsControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlockWars.GameState.Api.Unit.Tests.Controllers
+{
+    public class RealmsControllerTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ src/BlockWars.GameState.Api/Controllers/RealmsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+
+namespace BlockWars.GameState.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RealmsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
