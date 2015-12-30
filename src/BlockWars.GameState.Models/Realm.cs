40afe7acd4f878bb384a16eeb6cc9274a629d822
mtgorski <mgorski8@gmail.com>
2015-12-29 19:22:32 -0500

    Added validation attributes to realm model

--- src/BlockWars.GameState.Models/Realm.cs
+++ src/BlockWars.GameState.Models/Realm.cs
@@ -1,11 +1,19 @@
-﻿namespace BlockWars.GameState.Models
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace BlockWars.GameState.Models
 {
     public class Realm
     {
         public string RealmId { get; set; }
 
+        [Required]
+        [StringLength(256, MinimumLength = 1)]
         public string Name { get; set; }
 
+        [Required]
+        [StringLength(256, MinimumLength = 1)]
         public string Description { get; set; }
     }
 }
