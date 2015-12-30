3d4ce135f98e30d02db214f393a3d81062e6480d
mtgorski <mgorski8@gmail.com>
2015-12-29 23:39:29 -0500

    Missed files on last checkin

---
+++ src/BlockWars.GameState.Api/Automapper/RealmProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BlockWars.GameState.Api.DataModels;
+using BlockWars.GameState.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockWars.GameState.Api.Automapper
+{
+    public class RealmProfile : Profile
+    {
+        protected override void Configure()
+        {
+            base.Configure();
+
+            CreateMap<Realm, RealmData>();
+            CreateMap<RealmData, Realm>();
+        }
+    }
+}
