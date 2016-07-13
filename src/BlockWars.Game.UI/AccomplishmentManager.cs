a09ec76c126507d0543a1f6fbc011b4e72b597d4
mtgorski <mgorski8@gmail.com>
2016-07-13 07:54:39 -0400

    Changes accomplishments and styling

--- src/BlockWars.Game.UI/AccomplishmentManager.cs
+++ src/BlockWars.Game.UI/AccomplishmentManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace BlockWars.Game.UI.Actors
 {
@@ -7,12 +6,12 @@ namespace BlockWars.Game.UI.Actors
     {
         private Dictionary<int, Accomplishment> _thresholdToAccomplishment = new Dictionary<int, Accomplishment>
         {
-            {1, new Accomplishment { Text = "I - Baby Builder", Rank = 1} },
-            {10, new Accomplishment { Text = "II - Lego Builder", Rank = 2} },
-            {50, new Accomplishment { Text = "III - Mason", Rank = 3 } },
-            {100, new Accomplishment { Text = "IV - Master Builder", Rank = 4 } },
-            {314, new Accomplishment { Text = "V - Master Baker", Rank = 5} },
-            {500, new Accomplishment { Text = "VI - Monument Maker", Rank = 6 } },
+            {1, new Accomplishment { Text = "I - Lego Builder", Rank = 1} },
+            {10, new Accomplishment { Text = "II - House Builder", Rank = 2} },
+            {50, new Accomplishment { Text = "III - Skyscraper Builder", Rank = 3 } },
+            {100, new Accomplishment { Text = "IV - Monument Builder", Rank = 4 } },
+            {314, new Accomplishment { Text = "V - City Builder", Rank = 5} },
+            {500, new Accomplishment { Text = "VI - World Wonder Builder", Rank = 6 } },
             {1000, new Accomplishment { Text = "VII - World Builder", Rank = 7} }
         };
 
